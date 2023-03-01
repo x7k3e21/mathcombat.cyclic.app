164097cabacc3bdc9001e4d930df028790c1af55
tungsten-cat <126493730+tungsten-cat@users.noreply.github.com>
2023-03-01 06:28:56 +0000

    Initial commit

---
+++ application.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const application = express();
+
+application.get("/", (request, response) =>
+{
+	response.send("Yo!");
+});
+
+const server = application.listen(process.env.PORT || 3000, () =>
+{
+	console.log(`Server port: ${server.address().port});
+});
