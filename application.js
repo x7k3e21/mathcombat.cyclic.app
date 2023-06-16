970f4069bf4a6f13f5070901538e598c14e37702
tungsten-cat <uly4nkin.denis@gmail.com>
2023-06-16 22:45:36 +0300

    Added logging system

--- application.js
+++ application.js
@@ -1,6 +1,58 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+
+const environment = process.env.NODE_ENV;
+
+const winston_level = environment == "development" ? "debug" : "warn"; 
+
+const winston = require("winston");
+const winston_config = require("./winston");
+
+winston.addColors(winston_config.log_levels_colors);
+
+const winston_format = winston.format.combine
+(
+	winston.format.colorize({ all: true }),
+
+	winston.format.timestamp
+	({ 
+		format: "YYYY-MM-DD HH:mm:ss:ms" 
+	}),
+
+	winston.format.printf
+	(
+		(info) => `[${info.level}] ${info.timestamp}: ${info.message}`
+	)
+);
+
+const winston_transports =
+[
+	new winston.transports.Console(),
+	
+	new winston.transports.File
+	({
+		filename: "environment/logs/default.log"
+	}),
+
+	new winston.transports.File
+	({
+		filename: "environment/logs/errors.log", level: "error" 
+	})
+];
+
+const winston_logger = winston.createLogger
+({
+	level: winston_level,
+	format: winston_format,
+
+	transports: winston_transports,
+
+	levels: winston_config.log_levels_codes
+});
 
 const application = express();
+
+winston_logger.log("debug", "Express.js application was created!");
 
 application.get("/", (request, response) =>
 {
@@ -9,5 +61,6 @@ application.get("/", (request, response) =>
 
 const server = application.listen(process.env.PORT || 3000, () =>
 {
-	console.log(`Server port: ${server.address().port}`);
+	const server_host = server.address().address;
+	const server_port = server.address().port;
 });
---
+++ winston.js
@@ -0,0 +1,16 @@
+
+module.exports.log_levels_codes =
+{
+    error: 0,
+    warn: 1,
+    info: 2,
+    debug: 3
+};
+
+module.exports.log_levels_colors =
+{
+    error: "red",
+    warn: "yellow",
+    info: "green",
+    debug: "white"
+};
